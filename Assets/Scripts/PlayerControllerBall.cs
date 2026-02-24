d3dd5c36da357a74c42d29e9baa6c0e042dc21cd
milanjlin <milanjlin@berkeley.edu>
2026-02-24 10:48:25 -0800

    made a little bit neater

--- Assets/Scripts/PlayerControllerBall.cs
+++ Assets/Scripts/PlayerControllerBall.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerControllerBall : MonoBehaviour
-{
+public class PlayerControllerBall : MonoBehaviour {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-     private Rigidbody2D rb;
+    private Rigidbody2D rb;
+    
+    #region Jump Variables
     private bool canJump;
-
     [SerializeField] private float maxChargeTime;
     [SerializeField] private float jumpMultiplier;
     private float chargeTime;
     private bool isCharging = false;
+    #endregion
+
     [SerializeField] private float moveSpeed;
     float x_input;
     private bool touchingWall;
@@ -18,38 +20,31 @@ public class PlayerControllerBall : MonoBehaviour
     private bool isGrabbing;
     
    
-   void Start()
-    {
+   void Start() {
         rb = GetComponent<Rigidbody2D>();
         JumpSlider.value = 0;
     }
 
     // Update is called once per frame
-    void Update() 
-    {
+    void Update() {
         x_input = Input.GetAxisRaw("Horizontal");
         
-        if (Input.GetKey(KeyCode.Space) && canJump)
-        {
+        if (Input.GetKey(KeyCode.Space) && canJump) {
             isCharging = true;
-            if (chargeTime <= maxChargeTime)
-            {
+            if (chargeTime <= maxChargeTime) {
                 chargeTime += Time.deltaTime;
-                
             }
-            
         }
-        if (Input.GetKeyUp(KeyCode.Space) && isCharging)
-        {
+
+        if (Input.GetKeyUp(KeyCode.Space) && isCharging) {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, chargeTime * jumpMultiplier);
             isCharging = false;
             canJump = false;
             chargeTime = 0f;
             isGrabbing = false;
-            
         } 
-        if (Input.GetKey(KeyCode.S) && touchingWall)
-        {
+
+        if (Input.GetKey(KeyCode.S) && touchingWall) {
             isGrabbing = true;
         }
 
@@ -57,49 +52,38 @@ public class PlayerControllerBall : MonoBehaviour
         JumpSlider.value = chargeTime / maxChargeTime;
     
     }
-    void FixedUpdate()
-    {
-        if (isGrabbing)
-    {
+
+    void FixedUpdate() {
+        if (isGrabbing) {
         rb.gravityScale = 0f;
         rb.linearVelocity = Vector2.zero;
         rb.angularVelocity = 0f;
-    }
-    else
-    {
-        rb.gravityScale = 1f;
-    }
-        
+        }
+        else {
+            rb.gravityScale = 1f;
+        }  
     }
 
-    private void Move()
-{
-    if (isCharging || isGrabbing)
-        {
+    private void Move() {
+        if (isCharging || isGrabbing) {
             return;
         }
-        
-    rb.linearVelocity = new Vector2(x_input * moveSpeed, rb.linearVelocity.y);
-    
-}
+
+        rb.linearVelocity = new Vector2(x_input * moveSpeed, rb.linearVelocity.y);
+    }
     
     void OnTriggerStay2D (Collider2D coll) {
-        
-        if (coll.gameObject.CompareTag("Wall"))
-        {
+        if (coll.gameObject.CompareTag("Wall")) {
             canJump = true;
             touchingWall = true;
         }
     }
     
     
-    void OnTriggerExit2D (Collider2D coll)
-    {
-        if (coll.gameObject.CompareTag("Wall"))
-        {
+    void OnTriggerExit2D (Collider2D coll) {
+        if (coll.gameObject.CompareTag("Wall")) {
             canJump = false;
             touchingWall = false;
         }
     }
 }
-
